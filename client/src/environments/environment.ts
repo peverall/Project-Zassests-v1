0e493b8876a2892fb73135de039e7ec9ba4857d8
peverall <code.statera@gmail.com>
2023-08-30 21:35:02 -0700

    Added environments and Firebase code to environment.ts

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyAeHgGRjB_iz9q179MyNFUZS_HJwLUoEQg",
+        authDomain: "zassets-auth.firebaseapp.com",
+        projectId: "zassets-auth",
+        storageBucket: "zassets-auth.appspot.com",
+        messagingSenderId: "35348375614",
+        appId: "1:35348375614:web:1795ef0530e0d89175c96f"
+},
+
+production: false
+};
